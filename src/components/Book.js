d0faa8e36c26d4626911405ac39bac7aff6c9482
Jahongir Yusupov <91022355+jahongiry@users.noreply.github.com>
2022-07-13 15:52:00 +0500

    Use Redux in React components

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,10 +1,20 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removingAction } from '../redux/books/book';
 
-function Book({ title, author }) {
+function Book({ title, author, ids }) {
+  const dispatch = useDispatch();
+  const remove = (id) => {
+    dispatch(removingAction({ id }));
+  };
+
   return (
     <div>
       <p>{title}</p>
       <p>{author}</p>
+      <button onClick={() => remove(ids)} type="submit">
+        Remove
+      </button>
     </div>
   );
 }
@@ -12,6 +22,7 @@ function Book({ title, author }) {
 Book.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  ids: PropTypes.string.isRequired,
 };
 
 export default Book;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,36 @@
 // import { useState } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { bookList } from '../redux/books/book';
+import { useRef } from 'react';
+import { useDispatch } from 'react-redux';
+import nextId from 'react-id-generator';
+import { addingAction } from '../redux/books/book';
 
 function Form() {
+  const dispatch = useDispatch();
+  const titleInput = useRef(null);
+  const authorInput = useRef(null);
+  const addBook = (e) => {
+    e.preventDefault();
+    dispatch(
+      addingAction({
+        id: nextId(),
+        title: titleInput.current.value,
+        author: authorInput.current.value,
+      }),
+    );
+  };
   return (
     <form>
-      <input required id="title" placeholder="Book title" />
-      <input type="text" required id="author" placeholder="Book author" />
-      <button type="submit">Add book</button>
+      <input ref={titleInput} required id="title" placeholder="Book title" />
+      <input
+        ref={authorInput}
+        type="text"
+        required
+        id="author"
+        placeholder="Book author"
+      />
+      <button onClick={addBook} type="submit">
+        Add book
+      </button>
     </form>
   );
 }
